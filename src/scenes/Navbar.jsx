ee6266586d2e36a24c8ab7334af6530f0f3ab48d
Muhammad Junaid <648mjunaid@gmail.com>
2023-09-20 12:39:00 +0500

    add navbar links

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -4,11 +4,18 @@ import useMediaQuery from "../hooks/useMediaQuery";
 const Link = ({ page, selectedPage, setSelectedPage }) => {
   const lowerCasePage = page.toLowerCase();
   return (
-    <AnchorLink className={`${selectedPage === lowerCasePage}`}></AnchorLink>
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow " : ""}
+      hover:text-yellow transition duration-500`}
+      href={`${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
   );
 };
 const Navbar = ({ selectedPage, setSelectedPage }) => {
-  const [isMenuToggles, setIsMenuToggles] = useState(false);
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
 
   return (
@@ -18,9 +25,40 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
 
         {/* {DESKTOP NAV} */}
         {isAboveSmallScreens ? (
-          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold"></div>
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Testimonials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
         ) : (
-          <div></div>
+          <div>
+            <button className="rounded-full bg-red p-2"
+            onClick={()=>setIsMenuToggled(!isMenuToggled)}>
+              <img src="../assets/menu-icon.svg" alt="menu-icon" />
+            </button>
+          </div>
         )}
       </div>
     </nav>

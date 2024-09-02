30bb448a2a1b55c9febc14490d86ad0c5233f20d
Muhammad Junaid <648mjunaid@gmail.com>
2024-09-02 16:46:16 +0500

    updated linked url

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -3,7 +3,7 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start gap-7 my-5">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.linkedin.com/in/muhammad-junaid-350b79195"
+        href="https://www.linkedin.com/in/full-stack-web-developer-junaid/"
         target="_blank"
         rel="noreferrer"
       >
